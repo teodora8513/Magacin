3887c14abfd1b92ec35e965e2f386c9159ac3fe4
teodora8513 <jevtic.teodora@gmail.com>
2021-03-11 18:56:20 +0100

    Kreirana klasa MagacinServis koja nasledjuje interfejs

---
+++ src/servis/MagacinServis.java
@@ -0,0 +1,32 @@
+package servis;
+
+import interfejs.IMagacin;
+import magacin.Artikal;
+
+public class MagacinServis implements IMagacin {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dodajArtikalKolicinu(int sifra, int kol) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal izbaciArtikal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void pronadji(int sifra) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
